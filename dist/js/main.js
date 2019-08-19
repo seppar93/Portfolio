47f9799f986c99a9911633576fcf483bd7b088dc
Sepehr Parirokh <sepehrparirokh@gmail.com>
2019-08-18 17:31:51 -0700

    menu rotation

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,8 @@
-// class
+
+/**
+ * @description
+ * type writer class for the writing effect of the initatil page 
+ */
 class TypeWriter {
   constructor(txtElement, words, wait = 3000) {
     this.txtElement = txtElement;
@@ -43,11 +47,54 @@ class TypeWriter {
   }
 }
 
+
+// selecting DOM structure
+
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+
 // initiating the word setup and the class
 function init() {
   const txtElement = document.querySelector('.txt-type');
   const words = JSON.parse(txtElement.getAttribute('data-words'));
   const wait = txtElement.getAttribute('data-wait');
   new TypeWriter(txtElement, words, wait);
+}
+
+
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial State Of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add('close');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    menuBranding.classList.add('show');
+    navItems.forEach(item => item.classList.add('show'));
+
+    // Set Menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    menuBranding.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // Set Menu State
+    showMenu = false;
+  }
 }
